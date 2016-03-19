d382fbbdc09155b777521431e434430e6dcdc3c2
czbeatty <cameronzbeatty@gmail.com>
2016-03-19 17:56:28 -0400

    Autonomous modes

---
+++ AutonomousModes/AutonomousCrossCheval.java
@@ -0,0 +1,90 @@
+package org.usfirst.frc.team1188.robot.AutonomousModes;
+
+import org.usfirst.frc.team1188.robot.Calibrations;
+import org.usfirst.frc.team1188.robot.IAutonomousMode;
+import org.usfirst.frc.team1188.robot.Robot;
+
+public class AutonomousCrossCheval implements IAutonomousMode {
+	protected static final int driveToCheval = 1;
+	protected static final int wakeArm = 2;
+	protected static final int driveOverCheval = 3;
+	
+	protected static final int lowerArmToLowerCheval = 1;
+	protected static final int wakeDriveTrain = 2;
+	
+	public Robot robot;
+	
+	protected int driveFunction;
+	protected boolean driveWaiting;
+	
+	protected int armFunction;
+	protected boolean armWaiting;
+	
+	public AutonomousCrossCheval(Robot robot) {
+		this.robot = robot;
+	}
+	
+	public void init() {
+		driveFunction = AutonomousCrossCheval.driveToCheval;
+		driveWaiting = false;
+		
+		armFunction = AutonomousCrossCheval.lowerArmToLowerCheval;
+		armWaiting = true;
+	}
+		
+	public void maintainState() {
+		maintainDriveState();
+		maintainArmState();
+	}
+	
+	public void maintainDriveState() {
+		if (driveWaiting == false) {
+			switch (driveFunction) {
+			case AutonomousCrossCheval.driveToCheval:
+				robot.driveTrain.driveForwardInches(Calibrations.driveInchesToReachCheval, Calibrations.drivingForward, Calibrations.autonomousChevalSpeed);
+				break;
+			case AutonomousCrossCheval.wakeArm:
+				robot.driveTrain.stopAndWait();
+				robot.arm.wake();
+			case AutonomousCrossCheval.driveOverCheval:
+				robot.driveTrain.driveForwardInches(Calibrations.driveInchesToCrossCheval,  Calibrations.drivingForward, Calibrations.autonomousChevalCrossSpeed);
+			}
+			
+			driveWaiting = true;
+		}
+		
+		if (robot.driveTrain.automatedActionHasCompleted()) {
+			driveWaiting = false;
+			
+			// Incrementing the drive function pushes the program to the next state.
+			// Think of it like an array of actions (in fact, this *should* be a linked list or something instead.)
+			driveFunction++;
+		}
+		
+		robot.driveTrain.maintainState();
+	}
+	
+	public void maintainArmState() {
+		if (armWaiting == false) {
+			switch (armFunction) {
+				case AutonomousCrossCheval.lowerArmToLowerCheval:
+					robot.arm.moveArmToPosition(Calibrations.armDownTargetPosition);
+					break;
+				case AutonomousCrossCheval.wakeDriveTrain:
+					robot.driveTrain.wake();
+			}
+			
+			armWaiting = true;
+		}
+		
+		if (robot.arm.automatedActionHasCompleted()) {
+			armWaiting = false;
+			
+			// Incrementing the arm function pushes the program to the next state.
+			// Think of it like an array of actions (in fact, this *should* be a linked list or something instead.)
+			armFunction++;
+		}
+		
+		robot.arm.maintainState();
+	}
+}
---
+++ AutonomousModes/AutonomousCrossChevalTwice.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team1188.robot.AutonomousModes;
+
+import org.usfirst.frc.team1188.robot.IAutonomousMode;
+
+public class AutonomousCrossChevalTwice implements IAutonomousMode {
+	protected static final int driveToCheval = 1;
+	protected static final int driveOverCheval = 2;
+	protected static final int turnAround = 3;
+	protected static final int realignWithCheval = 4;
+	protected static final int driveOverCheval2 = 5;
+	protected static final int turnAround2 = 6;
+	
+	protected static final int lowerArmToLowerCheval = 1;
+	protected static final int raiseArmToClearCheval = 2;
+	protected static final int lowerArmToLowerCheval2 = 3;
+	protected static final int raiseArmToClearCheval2 = 4;
+	protected static final int lowerArmToLowerCheval3 = 5;
+	
+	public void init() {
+		
+	}
+	
+	public void maintainState() {
+		
+	}
+}
